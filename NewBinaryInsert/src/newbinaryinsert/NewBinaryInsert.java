3b1529c3c4467dc18108718ad0ff4e4b6521c4e9
IsuKay <IsuKay@IsuKay-PC>
2014-02-18 23:02:12 +0530

    find

---
+++ NewBinaryInsert/src/newbinaryinsert/NewBinaryInsert.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NewBinaryInsert;
+
+/**
+ *
+ * @author IsuKay
+ */
+public class NewBinaryInsert{
+               NewBinaryInsert.Node root;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         NewBinaryInsert theTree = new NewBinaryInsert();
+              System.out.println("Search for 20");
+               System.out.println(theTree.findNode(20));
+            
+               
+               
+    }
+    
+     class Node
+ 
+ {
+  int key;
+  String name;
+  
+  NewBinaryInsert.Node LeftChild;
+  NewBinaryInsert.Node RightChild;
+  
+  // Create Constructor For assign values
+  Node(int key , String name)
+  {
+   this.key=key;
+   this.name = name;
+   
+  }
+  
+  public String toString()
+  {
+   return name + " book price is  " + key;
+     
+  }
+  
+  }
+    
+    public NewBinaryInsert.Node findNode(int key)
+ {
+  
+  NewBinaryInsert.Node focusNode = root;
+  
+  while(focusNode.key != key)
+  {
+   if(key < focusNode.key)
+   {
+    
+    focusNode = focusNode.LeftChild;
+    
+   }
+   else
+   {
+    focusNode = focusNode.RightChild;
+    
+   }
+   
+   if(focusNode == null)
+   {
+    return null;
+   }
+  }
+                   return null;
+}
+}
